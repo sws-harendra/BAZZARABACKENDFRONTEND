af5d369eb369c615efa26775f4be399e82b508c8
Sahil-9619 <sahilkumar242004@gmail.com>
2026-04-01 17:40:18 +0530

    ,

--- frontend/app/(user)/components/footer.tsx
+++ frontend/app/(user)/components/footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
     fetchLinks();
   }, []);
   return (
-    <footer className=" bg-white text-black">
+    <footer className=" bg-white text-black border-t border-gray-300">
       <div className="max-w-7xl mx-auto px-6 py-14 grid grid-cols-1 md:grid-cols-4 gap-10">
         {/* Brand */}
         <div>
