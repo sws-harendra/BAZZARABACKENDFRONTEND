2447124a0ec4d1877ecdc69c310bcc1761b4c5ae
Sahil-9619 <sahilkumar242004@gmail.com>
2026-04-07 13:37:22 +0530

    fixed again

--- backend/migrations/20260331122846-update-user-table.js
+++ backend/migrations/20260331122846-update-user-table.js
@@ -3,12 +3,6 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
 
-        // ✅ email unique constraint
-        await queryInterface.changeColumn('Users', 'email', {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true,
-        });
         await queryInterface.addColumn('Users', 'resetPasswordToken', {
             type: Sequelize.STRING,
             allowNull: true,
@@ -19,12 +13,5 @@ module.exports = {
     async down(queryInterface, Sequelize) {
 
         await queryInterface.removeColumn('Users', 'resetPasswordToken');
-        await queryInterface.removeColumn('Users', 'resetPasswordExpire');
-
-        await queryInterface.changeColumn('Users', 'email', {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: false,
-        });
     },
 };
